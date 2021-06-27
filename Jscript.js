b4ccea90962389f54b534703a7c5422fe13d5db5
patiencekorrie <patience_korrie@taylor.edu>
2021-06-27 08:55:41 -0400

    update
    added new files

--- Jscript.js
+++ Jscript.js
@@ -1,1 +1,12 @@
-fountion()
+var video = document.getElementById("myVideo");
+var btn = document.getElementById("myBtn");
+
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
