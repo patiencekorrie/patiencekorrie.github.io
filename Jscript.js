a6449849c13bdf663864d817ea28ee1859ce778c
patiencekorrie <patience_korrie@taylor.edu>
2021-07-14 23:29:14 -0400

    mades lots of changes

--- Jscript.js
+++ Jscript.js
@@ -17,3 +17,11 @@
 
 //     new WOW().init();
 
+let btn = document.querySelector('.mouse-cursor-gradient-tracking');
+btn.addEventListener('mousemove', e => {
+  let rect = e.target.getBoundingClientRect();
+  let x = e.clientX - rect.left;
+  let y = e.clientY - rect.top;
+  btn.style.setProperty('--x', x + 'px');
+  btn.style.setProperty('--y', y + 'px');
+});
