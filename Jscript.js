ddda82001b055ac13055c3cd82ffbef6fa5609e2
patiencekorrie <patience_korrie@taylor.edu>
2021-07-23 11:32:40 -0400

    updated images

--- Jscript.js
+++ Jscript.js
@@ -1,27 +1,4 @@
-// var video = document.getElementById("myVideo");
-// var btn = document.getElementById("myBtn");
 
-// function myFunction() {
-//   if (video.paused) {
-//     video.play();
-//     btn.innerHTML = "Pause";
-//   } else {
-//     video.pause();
-//     btn.innerHTML = "Play";
-//   }
-// }
-
-
-
-// $(".img-fluid").addClass("wow fadeIn z-depth-1-half");
-
-//     new WOW().init();
-
-let btn = document.querySelector('.mouse-cursor-gradient-tracking');
-btn.addEventListener('mousemove', e => {
-  let rect = e.target.getBoundingClientRect();
-  let x = e.clientX - rect.left;
-  let y = e.clientY - rect.top;
-  btn.style.setProperty('--x', x + 'px');
-  btn.style.setProperty('--y', y + 'px');
-});
+let el = document.querySelector('.el');
+let height = el.scrollHeight;
+el.style.setProperty('--max-height', height + 'px');
